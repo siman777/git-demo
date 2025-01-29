b6dad11beda74a87f37c65752ca253e4e7361f92
simangit <simanraj9838@gmail.com>
2025-01-29 18:16:06 +0530

    code change

--- new.cpp
+++ new.cpp
@@ -1,7 +1,20 @@
-#include<bits/stdc++.h>
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include<vector>
 using namespace std;
 
+bool palindrome(string s,int start,int end){
+    if(start>=end) return true;
+    if(s[start]!=s[end]) return false;
+    return palindrome(s,start+1,end-1);
+    
+}
 int main(){
-    cout<<"hello world";
-    return 0;
+    //sum of given number by using recusion
+   string s="";
+   cout<<"enter the string :";
+   cin>>s;
+   if(palindrome(s,0,s.size()-1)) cout<<"string is plaindrome";
+   else cout<<"string is not palindrome";
+    
 }
